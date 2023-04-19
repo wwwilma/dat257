3177a1ecd0a0756f076492cd50a9354cf083c8be
paob02 <paulineb@student.chalmers.se>
2023-04-19 17:36:23 +0200

    loggan fungerar och ser bra ut

--- dat257/src/Header.js
+++ dat257/src/Header.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import './Header.css'
-import logo from './img/TRACK_logo.png'
+
 
 
 function Header() {
     return (
         <div className="Header">
-            <img src={logo} className="App-logo" alt="logo"
-                 style={{
-                     width: 250,
-                     height: 59
-                 }}
-            />
+            <h1>TRACK fast logo</h1>
         </div>
 
     )
