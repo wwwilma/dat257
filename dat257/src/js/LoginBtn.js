e82ee2114cf6af7db0049062822d2d4e71c8bc6b
Hulling <hullingk@student.chalmers.se>
2023-05-15 15:22:20 +0200

    add btn for new user

--- dat257/src/js/LoginBtn.js
+++ dat257/src/js/LoginBtn.js
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from "react";
 import "../css/Login.css";
 import axios from "axios";
 import {useNavigate } from 'react-router-dom';
-
+import plusSolid from "../img/plus-solid.svg";
 // Buttons for usernames on login page.
 export default function LoginBtn({onUserChange}) {
     const [users, setUsers] = useState([]);
     const navigate = useNavigate();
+    const [newUserName, setNewUserName] = useState('');
+    const [showInputField, setShowInputField] = useState(false);
 
     // When the login page is loaded it will retrieve the users from the database and
     // set them in users.
@@ -25,18 +27,71 @@ export default function LoginBtn({onUserChange}) {
         onUserChange(newUserID)
         navigate('/Home');
     }
-
+    function handleAddUser() {
+        if (newUserName.trim() !== '') {
+            const newUser = {
+                id: 1,
+                name: newUserName.trim()
+            };
+            setUsers(prevUsers => [...prevUsers, newUser]);
+            setNewUserName('');
+            setShowInputField(false);
+        }
+    }
+    function renderAddUserButton() {
+        if (showInputField) {
+            return (
+                    <h1 className="userNameBtn">
+                        <input
+                            className="input"
+                            type="text"
+                            value={newUserName}
+                            onChange={(e) => setNewUserName(e.target.value)}
+                            onKeyDown={event => {
+                                if (event.key === 'Enter') {
+                                handleAddUser()
+                            }
+                            }}
+                            placeholder="Enter user name"
+                        />
+                        <img
+                            onClick={handleAddUser}
+                            src={plusSolid}
+                            className="plusSolidImg"
+                        />
+                    </h1>
+            );
+        } else {
+            return (
+                <h1
+                    className="userNameBtn"
+                    onClick={() => setShowInputField(true)}
+                >
+                    <span>Add new user</span>
+                    <img
+                        src={plusSolid}
+                        className="plusSolidImg"
+                    />
+                </h1>
+            );
+        }
+    }
 
     return (
-        <div className= "containerLogin">
+        <div className="containerLogin">
             {users.map(user => (
-            <h1 key={user.id}
-                className="userNameBtn"
-                onClick={() => handleUserChange(user.id)}>
-                {user.name}
-            </h1>))}
+                <h1
+                    key={user.id}
+                    className="userNameBtn"
+                    onClick={() => handleUserChange(user.id)}
+                >
+                    {user.name}
+                </h1>
+            ))}
+            {renderAddUserButton()}
         </div>
     );
+
 
 
 }
