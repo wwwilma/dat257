3d8ea0a4a63e8fca948ca0f1df24e2b3b9432918
Isak <isakwideskott@gmail.com>
2023-05-16 14:48:20 +0200

    Fix

--- dat257/src/js/LoginBtn.js
+++ dat257/src/js/LoginBtn.js
@@ -30,6 +30,7 @@ export default function LoginBtn({onUserChange}) {
     function handleAddUser() {
         if (newUserName.trim() !== '') {
             const newUser = {
+                id: 1,
                 name: newUserName.trim()
             };
             setUsers(prevUsers => [...prevUsers, newUser]);
