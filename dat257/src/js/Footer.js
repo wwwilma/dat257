30edb722df9713bbe3256a1d1eb84ef9b6c2bfc4
paob02 <paulineb@student.chalmers.se>
2023-05-16 10:52:57 +0200

    030 and 032 user stories works

--- dat257/src/js/Footer.js
+++ dat257/src/js/Footer.js
@@ -4,7 +4,7 @@ import '../css/Footer.css'
 function Footer() {
     return (
         <div className="Footer">
-            <h1>Footer</h1>
+
         </div>
     )
 }
--- dat257/src/js/Header.js
+++ dat257/src/js/Header.js
@@ -2,26 +2,43 @@ import React from 'react'
 import '../css/Header.css'
 import '../css/Profile.css'
 import logo from '../img/Track_logo.png'
-import profilelogo from '../profile.svg'
+import switchuserlogo from '../img/switchuserlogo.png'
+import {useNavigate } from 'react-router-dom';
 
 
 
 function Header() {
 
+    const navigate = useNavigate();
+
+    /* Navigates the user to "All users"-page when clicking "switch user"-icon */
+    function onClickswitchuser(event){
+        navigate("/")
+        event.stopPropagation();
+
+    }
+
+    /* Navigates the user to "Home"-page when clicking "Track"-logo */
+    function onClicktracklogo(){
+        navigate("/Home")
+    }
+
+
     return (
         <div className="Header">
-            <img src={logo} className="App-logo" alt="logo"
+            <img src={logo} className="App-logo" alt="logo" onClick={onClicktracklogo}
                  style={{
                      width: 250,
                      height: 59
                  }}
             />
-            <img src={profilelogo} className="profile-logo" alt="profilelogo"/>
+
+            <img src={switchuserlogo} className="profile-logo" alt="profilelogo"/>
 
             <div className="dropdown">
-                <button className="dropbtn"></button>
+                <button className="dropbtn" onClick={onClickswitchuser}></button>
                 <div className="dropdown-content">
-                    <a href="/" >Change user</a>
+                    <a>Change user</a>
                 </div>
             </div>
         </div>
