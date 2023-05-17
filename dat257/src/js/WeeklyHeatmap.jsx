eb47813f6df164ea63fbbc1f476412f2ec79ec9f
emmaaxelsso <93664659+emmaaxelsso@users.noreply.github.com>
2023-05-17 11:55:30 +0200

    Labels for the panel colors, all month labels is visible. Small changes in size. Co-author: Lovisa

--- dat257/src/js/WeeklyHeatmap.jsx
+++ dat257/src/js/WeeklyHeatmap.jsx
@@ -16,22 +16,23 @@ const WeeklyHeatmap = ({statistics}) => {
                 value={statistics}
                 startDate={weekStart}
                 endDate={weekEnd}
-                style={{ color: '#35524A' }}
+                style={{ color: '#000000' }}
                 rectSize={14}
-                legendCellSize={0}
+                legendCellSize={35}
                 space={2}
                 panelColors={{
                     1: '#C4B1E7',
-                    6: '#A183D8',
-                    11: '#7D54C9',
-                    21: '#5F36AB',
-                    31: '#45277C',
-                    41: '#2B184E',
-                    101: '#110A1F',
+                    5: '#A183D8',
+                    10: '#7D54C9',
+                    20: '#5F36AB',
+                    30: '#45277C',
+                    40: '#2B184E',
+                    100: '#110A1F',
                 }}
                 weekLabels={['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']}
                 monthLabels={{Boolean:false}}
             />
+            <small>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 0 &nbsp; &nbsp; &nbsp; 1 &nbsp; &nbsp; &nbsp; 5 &nbsp; &nbsp; &nbsp; 10 &nbsp; &nbsp; 20 &nbsp; &nbsp; 30 &nbsp; &nbsp; 40 </small>
         </div>
     )
 };
--- dat257/src/js/Yearlyheatmap.jsx
+++ dat257/src/js/Yearlyheatmap.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import HeatMap from '@uiw/react-heat-map';
 import {endOfYear, startOfYear} from 'date-fns'
 
@@ -14,24 +13,25 @@ const YearlyHeatmap = ({statistics}) => {
             <h3>Yearly statistics</h3>
             <HeatMap
                 value={statistics}
-                startDate={yearStart}
+                startDate={new Date(yearStart.getTime()-(24 * 60 * 60 * 1000))}
                 endDate={yearEnd}
-                width={710}
+                width={890}
                 space={2}
                 rectSize={14}
-                legendCellSize={20}
-                style={{ color: '#35524A' }}
+                legendCellSize={35}
+                style = {{ color: '#000000' }}
                 monthLabels={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}
                 panelColors={{
                     1: '#C4B1E7',
-                    6: '#A183D8',
-                    11: '#7D54C9',
-                    21: '#5F36AB',
-                    31: '#45277C',
-                    41: '#2B184E',
-                    101: '#110A1F',
-                }} //Vill vi lägga till tooltip?
+                    5: '#A183D8',
+                    10: '#7D54C9',
+                    20: '#5F36AB',
+                    30: '#45277C',
+                    40: '#2B184E',
+                    100: '#110A1F',
+                }}
             />
+            <small>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 0 &nbsp; &nbsp; &nbsp; 1 &nbsp; &nbsp; &nbsp; 5 &nbsp; &nbsp; &nbsp; 10 &nbsp; &nbsp; 20 &nbsp; &nbsp; 30 &nbsp; &nbsp; 40 </small>
         </div>
     )
 };
